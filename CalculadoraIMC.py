a8c60747b00bec6d8ca25ff684384506781f78e2
Marco-Castillo <marco162821@gmail.com>
2024-08-02 20:54:51 -0600

    Se agregan comentarios, saltos de linea, se cambian algunas variables de lugar

--- CalculadoraIMC.py
+++ CalculadoraIMC.py
@@ -1,11 +1,10 @@
 def principal():
+    #se solicita datos al usuario y se guardan en variables(nombre, apellido paterno y apellido materno)
     nombre = (input("Ingrese su nombre: "))
     apellidoP = input("Ingrese su apellido paterno: ")
     apellidoM = input("Ingrese su apellido materno: ")
-    edad = int(input("ingresa tu edad: "))
-    estatura = float (input("ingresa tu estatura en metros: "))
-    peso = float(input("ingresa tu peso en kilogramos: "))
 
+    #si nombre, apellidop o apellidoM estan vacíos el programa imprime un mensaje de error y vuelve a llamar la funcion principal
     if nombre == "":
         print ("Intentalo de nuevo, llena correctamente tu nombre")
         return principal()
@@ -15,29 +14,39 @@ def principal():
     if apellidoM == "":
         print ("Intentalo de nuevo, llena correctamente tu Apellido Materno")
         return principal()
+   
+    #se solicita datos al usuario y se guardan en variables(edad, estatura y peso)
+    edad = int(input("ingresa tu edad: "))
+    estatura = float (input("ingresa tu estatura en metros: "))
+    peso = float(input("ingresa tu peso en kilogramos: "))
 
+    #Se crea una variable de nombre estatura 2 para despues hacer la operación de estatura al cuadrado
     estatura2 = estatura**2
+   
+    #Se calcula el IMC multiplicando la variable "peso" por la variable que se acaba de crear "estatura2" y se guarda en una variable llamada IMC
     imc = peso/estatura2
-    
-    print(f"Tu nombre completo es : "+ nombre + " " + apellidoP + " " + apellidoM)
-
+   
+    #se imprime el nombre completo, concatenando las variables de nombre, apellidoP y apellidoM
+    print(f"\n\n\n\nTu nombre completo es : "+ nombre + " " + apellidoP + " " + apellidoM)
+   
+    #se crea una condición la cual evalua la edad para arrojar un mensaje y decir si eres menor o mayor de edad
     if (edad < 18):
-        print("Niñ@ todavía te piden la autorización de un adulto")
+        print(f"{edad} años, niñ@ todavía te piden la autorización de un adulto")
     else:
-        print("¡Ya estas en la edad del mundo de las facturas y los impuestos!")
-    
-    print(f"Tu peso es : {peso}")
-    print(f"Tu estatura es : {estatura}")
+        print(f"{edad} años, ya estas en la edad del mundo de las facturas y los impuestos")
+   
+    #se imprime los datos que se solicitaron en un inicio "peso" "estatura"
+    print(f"Tu peso es : {peso} kg.")
+    print(f"Tu estatura es : {estatura} m.")
+   
+    #se crea una condición para arrojar un mensaje dependiendo de el resultado que se obtuvo al realizar el calculo de IMC
     if (imc >= 0 and imc<=18.5):
-        print("Bajo de peso")
+        print(f"Tu IMC es de {imc}, precaución estas por de bajo de tu peso\n\n")
     elif (imc >= 18.5 and imc<=24.9):
-        print("Tu peso es ideal")
+        print(f"Tu IMC es de {imc}, Muy bien te encuentras en tu peso ideal\n\n")
     elif (imc >= 25 and imc<=29.9):
-        print("Tienes Sobrepeso")
+        print(f"Tu IMC es de {imc}, lo que indica que tienes sobrepeso\n\n")
     elif (imc >= 30):
-        print("Estas Obesoooooooo")
-    
-    
-
-    
+        print(f"Tu IMC es de {imc}, Mucho cuidado tienes obesidad\n\n")
+        
 principal()
